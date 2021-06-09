5398a7c1c85974367cf8168200168ec35e16a841
jihyeon-e <jihyeon-e@gmail.com>
2021-06-09 15:03:22 +0900

    member

---
+++ src/main/java/member/bean/MemberDTO.java
@@ -0,0 +1,14 @@
+package member.bean;
+
+import java.sql.Date;
+
+public class MemberDTO {
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private String phone;
+	private String address;
+	private Date birthday;
+	private Date created_at;
+}
